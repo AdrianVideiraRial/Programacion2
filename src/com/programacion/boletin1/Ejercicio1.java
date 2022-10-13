df75d5443e7286b9803f4e6f869df3d8bd5b475d
Adrian <avideirarial@danielcastelao.org>
2022-10-13 20:57:07 +0200

    Second commit

---
+++ src/com/programacion/boletin1/Ejercicio1.java
@@ -0,0 +1,8 @@
+package com.programacion.boletin1;
+
+public class Ejercicio1 {
+    public static void  main(String[] arg){
+        System.out.println("Hello World");
+
+    }
+}
---
+++ src/com/programacion/boletin2/CuentaAtras.java
@@ -0,0 +1,7 @@
+package com.programacion.boletin2;
+
+public class CuentaAtras {
+    public static void  main(String[] arg){
+
+    }
+}
